b03aa966473ddb7ce1b672cf4c9f9a7ca9ca5f43
a24rubenpv <rubenpeon.leo@gmail.com>
2024-09-26 14:33:16 +0200

    Segundo commit

---
+++ README.md
@@ -0,0 +1,5 @@
+A person wants to reduce the number of candles to be used to celebrate their birthday.
+To achieve this, the person is going to set their new age in binary.
+Each candle will represent one bit, and the candle will be lit to represent a 1, or extinguished to indicate a 0.
+As a result, not only will she use far fewer candles, they won't even have to light them all!
+The person is so confident that she will finally be able to blow out all the candles that they are even determined to put their age in seconds.
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,23 @@
 package org.example;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * It receives a number input by the keyboard and transform to binary
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write your age: ");
+        int age = sc.nextInt();
+        int numeroVelas = 0;
+        String bin = Integer.toBinaryString(age);
+
+        for (int i = 0; i < bin.length(); i++) {
+            if (bin.charAt(i) == '1'){
+                numeroVelas++;
+            }
         }
+        System.out.println(numeroVelas);
     }
 }
